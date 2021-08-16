2e6057f7c910e28fd436e01b2b8dc2caf8c595ba
Zeqiang Lai <916323301@qq.com>
2021-08-16 15:13:56 +0800

    update scatter plot

--- scatter/scatter_plot.py
+++ scatter/scatter_plot.py
@@ -1,57 +1,60 @@
 import matplotlib.pyplot as plt
 
-gaussian = [
-    (38.45, 154, 'BM4D', 20, 0.1),
-    (41.48, 1755, 'KBR', -350, -0.7),
-    (42.62, 1600, 'WLRTR', -600, 0.3),
-    (42.99, 166, 'NGmeet',-50, -0.7),
-    (38.70, 3, 'HSCNN', 0.5, 0.1),
-    (42.22, 0.8, 'QRNN3D', -0.1, -0.6),
-    (43.06, 1, 'Ours', 0.2, 0),
-]
 
-complex = [
-    (32.80, 6, 'LRMR', 1, 0.1),
-    (33.62, 88, 'LRTV', 15, 0),
-    (34.51, 170, 'NMoG', 25, 0.1),
-    (38.14, 231, 'TDTV', 45, -0.2),
-    (38.40, 3, 'HSCNN', 0.5, -0.2),
-    (42.79, 0.8, 'QRNN3D', 0.1, -0.5),
-    (42.89, 1, 'Ours', 0.1, -0.2)
-]
+def plot_scatter(ax, data, **kwargs):
+    """ plot scatter graph with annoation for each point
 
-inpainting = [
-    (41.90, 500, 'WLRTR', -300, -0.5),
-    (39.10, 40, 'FastHy', 5, 0),
-    (42.43, 100, 'Ours', 10, 0),
-]
+        Usage:
+        ```
+        fig, ax = plt.subplots()
+        gaussian = [
+            (154, 38.45, 'BM4D', 20, 0.1),
+            (1755, 41.48, 'KBR', -350, -0.7),
+        ]
+        plot_scatter(ax, gaussian, color='red', alpha=0.6, label='Gaussian')    
+        ```
+    Args:
+        ax: matplotlib axes object
+        data: list of description for each point in the format of (x, y, name, xtext, ytext) 
+        kwargs: you can pass any args of plt.scatter to this function.
+    """
+    
+    x, y, name, xt, yt = zip(*data)
+    ax.scatter(x, y, **kwargs)
+    for i in range(len(x)):
+        plt.annotate(name[i], xy=(x[i], y[i]), xytext=(x[i]+xt[i], y[i]+yt[i]))
 
-sr = [
-    (39.8, 0.7, '3D-FCNN', 0.1, 0),
-    (47.5, 4.4, 'SSPSR', 0.5, 0),
-    (41.1, 0.3, 'IFN', 0.1, 0),
-    (42.5, 1.8, 'Bi-3DQRNN', 0.1, 0),
-    (48.8, 25, 'Ours', -13, 0)
-]
+
+## Exampel usages
 
 def denoise():
+    gaussian = [
+        (154, 38.45, 'BM4D', 20, 0.1),
+        (1755, 41.48, 'KBR', -350, -0.7),
+        (1600, 42.62,  'WLRTR', -600, 0.3),
+        (166, 42.99,  'NGmeet', -50, -0.7),
+        (3, 38.70, 'HSID-CNN', 0.5, 0.1),
+        (0.8, 42.22,  'QRNN3D', -0.1, -0.6),
+        (1, 43.06,  'Ours', 0.2, 0),
+    ]
+
+    complex = [
+        (6, 32.80, 'LRMR', 1, 0.1),
+        (88, 33.62,  'LRTV', 15, 0),
+        (170, 34.51,  'NMoG', 25, 0.1),
+        (231, 38.14, 'TDTV', 45, -0.2),
+        (3, 38.40,  'HSID-CNN', 0.5, -0.2),
+        (0.8, 42.79,  'QRNN3D', 0.1, -0.5),
+        (1, 42.89,  'Ours', 0.1, -0.2)
+    ]
+    
     fig, ax = plt.subplots()
 
-    psnr, time, name, xt, yt = zip(*gaussian)
-    x, y = time, psnr
-    ax.scatter(x, y, color='red', alpha=0.6, label='Gaussian')
-    for i in range(len(x)):
-        plt.annotate(name[i], xy = (x[i], y[i]), xytext = (x[i]+xt[i], y[i]+yt[i]))
-        
-    psnr, time, name, xt, yt = zip(*complex)
-    x, y = time, psnr
-    ax.scatter(x, y, color='blue', alpha=0.6, label='Complex')
-    for i in range(len(x)):
-        plt.annotate(name[i], xy = (x[i], y[i]), xytext = (x[i]+xt[i], y[i]+yt[i]))
-        
+    plot_scatter(ax, gaussian, color='red', alpha=0.6, label='Gaussian')
+    plot_scatter(ax, complex, color='blue', alpha=0.6, label='Complex')
+
     ax.legend()
-    # ax.grid(True)
-    
+
     plt.title('Denoising Task')
     plt.xlabel('Running Time (sec)')
     plt.ylabel('PSNR (dB)')
@@ -59,24 +62,29 @@ def denoise():
     plt.savefig('denoise.pdf')
     plt.show()
 
+
 def sr_inpainting():
+    inpainting = [
+        (500, 41.90, 'WLRTR', -300, -0.5),
+        (40, 39.10,  'FastHyIn', 5, 0),
+        (100, 42.43,  'Ours', 10, 0),
+    ]
+
+    sr = [
+        (0.7, 39.8, '3D-FCNN', 0.1, 0),
+        (4.4, 47.5,  'SSPSR', 0.5, 0),
+        (0.3, 41.1, 'IFN', 0.1, 0),
+        (1.8, 42.5, 'Bi-3DQRNN', 0.1, 0),
+        (25, 48.8,  'Ours', -13, 0)
+    ]
+
     fig, ax = plt.subplots()
 
-    psnr, time, name, xt, yt = zip(*sr)
-    x, y = time, psnr
-    ax.scatter(x, y, color='red', alpha=0.6, label='Super Resolution')
-    for i in range(len(x)):
-        plt.annotate(name[i], xy = (x[i], y[i]), xytext = (x[i]+xt[i], y[i]+yt[i]))
-        
-    psnr, time, name, xt, yt = zip(*inpainting)
-    x, y = time, psnr
-    ax.scatter(x, y, color='blue', alpha=0.6, label='Inpainting')
-    for i in range(len(x)):
-        plt.annotate(name[i], xy = (x[i], y[i]), xytext = (x[i]+xt[i], y[i]+yt[i]))
-        
+    plot_scatter(ax, sr, color='red', alpha=0.6, label='Super Resolution')
+    plot_scatter(ax, inpainting, color='blue', alpha=0.6, label='Inpainting')
+
     ax.legend()
-    # ax.grid(True)
-    
+
     plt.title('Super Resolution & Inpainting Tasks')
     plt.xlabel('Running Time (sec)')
     plt.ylabel('PSNR (dB)')
@@ -84,5 +92,7 @@ def sr_inpainting():
     plt.savefig('sr_inpaint.pdf')
     plt.show()
 
-# denoise()
-sr_inpainting()
+
+if __name__ == '__main__':
+    # denoise()
+    sr_inpainting()
